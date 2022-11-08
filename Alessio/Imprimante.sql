8238428af29f4bafc896b5f66317f3a70568ef08
Alescor3 <111990233+Alescor3@users.noreply.github.com>
2022-11-08 15:22:10 +0100

    Update Imprimante.sql

--- Alessio/Imprimante.sql
+++ Alessio/Imprimante.sql
@@ -12,8 +12,8 @@
 -- Database Section
 -- ________________ 
 
-create database Imprimande;
-use Imprimande;
+create database db_Imprimante;
+use db_Imprimante;
 
 
 -- Tables Section
@@ -21,37 +21,40 @@ use Imprimande;
 
 create table t_client (
      idClient int not null auto_increment,
-     cliNom char(1) not null,
-     cliTel char(1) not null,
-     cliAdresse char(1) not null,
+     cliNom varchar(50) not null,
+	 cliPrenom varchar(50) not null,
+     cliTel varchar(20) not null,
+     cliAdresse char(255) not null,
+	 cliVille varchar(90) not null,
      constraint ID_t_client_ID primary key (idClient));
 
 create table t_commander (
      idClient int not null,
      idImprimante int not null,
      comDate date not null,
-     comPrix char(1) not null,
+     comPrix intnot null,
      constraint ID_t_commander_ID primary key (idImprimante, idClient));
 
 create table t_imprimante (
      idImprimante int not null auto_increment,
      impHauteur int not null,
      impLargeur int not null,
-     impModele char(1) not null,
+     impModele varchar(50) not null,
+	 impNom varchar(50) not null,
      impVitesse int not null,
      impResolution int not null,
      impRectoVerso char not null,
      impBacPapier int not null,
-     impPrix char(1) not null,
-     impPrixInitial char(1) not null,
+     impPrix int not null,
+     impPrixInitial int not null,
      idFabriquant int not null,
      constraint ID_t_imprimante_ID primary key (idImprimante));
 
 create table t_fabriquant (
      idFabriquant int not null auto_increment,
-     fabNom char(1) not null,
-     fabTel char(1) not null,
-     fabAdresse char(1) not null,
+     fabNom varchar(50) not null,
+     fabTel varchar(20) not null,
+     fabAdresse char(255) not null,
      constraint ID_t_fabriquant_ID primary key (idFabriquant));
 
 
