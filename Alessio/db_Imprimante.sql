f8c9f27c537899f227872fb9e6ba7dee486a1099
Alescor3 <111990233+Alescor3@users.noreply.github.com>
2022-11-22 15:38:57 +0100

    MCD MLD MPD

--- Alessio/db_Imprimante.sql
+++ Alessio/db_Imprimante.sql
@@ -3,9 +3,9 @@
 -- *--------------------------------------------
 -- * DB-MAIN version: 11.0.2              
 -- * Generator date: Sep 14 2021              
--- * Generation date: Tue Nov 22 14:06:42 2022 
+-- * Generation date: Tue Nov 22 14:41:40 2022 
 -- * LUN file: C:\Users\pa44ulq\Desktop\git\GesProj2-Imprimante-AGM\Alessio\Imprimante.lun 
--- * Schema: db_Imprimante/MCD 
+-- * Schema: db_Imprimante/MLD 
 -- ********************************************* 
 
 
@@ -25,20 +25,28 @@ create table t_client (
      cliPrenom varchar(50) not null,
      cliTel varchar(20) not null,
      cliAdresse char(255) not null,
-     constraint IDt_client primary key (idClient));
+     constraint ID_t_client_ID primary key (idClient));
+
+create table t_commander (
+     idCommander int not null,
+     comDate date not null,
+     comPrix int not null,
+     idClient int not null,
+     idImprimante int not null,
+     constraint ID_t_commander_ID primary key (idCommander));
 
 create table t_consommable (
      idConsommable int not null auto_increment,
      conNom varchar(100) not null,
      conPrix int not null,
-     constraint IDt_consommable primary key (idConsommable));
+     constraint ID_t_consommable_ID primary key (idConsommable));
 
 create table t_fabriquant (
      idFabriquant int not null auto_increment,
      fabNom varchar(50) not null,
      fabTel varchar(20) not null,
      fabMail char(255) not null,
-     constraint IDt_fabriquant primary key (idFabriquant));
+     constraint ID_t_fabriquant_ID primary key (idFabriquant));
 
 create table t_imprimante (
      idImprimante int not null auto_increment,
@@ -56,13 +64,66 @@ create table t_imprimante (
      impDisponibilite char not null,
      impPrix int not null,
      impPrixInitial int not null,
-     constraint IDt_imprimante primary key (idImprimante));
+     idFabriquant int not null,
+     constraint ID_t_imprimante_ID primary key (idImprimante));
+
+create table t_utiliser (
+     idConsommable int not null,
+     idImprimante int not null,
+     constraint ID_t_utiliser_ID primary key (idConsommable, idImprimante));
 
 
 -- Constraints Section
 -- ___________________ 
 
+alter table t_commander add constraint FKt_c_t_i
+     foreign key (idImprimante)
+     references t_imprimante (idImprimante);
+
+alter table t_commander add constraint FKt_c_t_c_FK
+     foreign key (idClient)
+     references t_client (idClient);
+
+alter table t_imprimante add constraint FKt_creer_FK
+     foreign key (idFabriquant)
+     references t_fabriquant (idFabriquant);
+
+alter table t_utiliser add constraint FKt_u_t_i_FK
+     foreign key (idImprimante)
+     references t_imprimante (idImprimante);
+
+alter table t_utiliser add constraint FKt_u_t_c
+     foreign key (idConsommable)
+     references t_consommable (idConsommable);
+
 
 -- Index Section
 -- _____________ 
+
+create unique index ID_t_client_IND
+     on t_client (idClient);
+
+create unique index ID_t_commander_IND
+     on t_commander (idImprimante, idClient, idCommander);
+
+create index FKt_c_t_c_IND
+     on t_commander (idClient);
+
+create unique index ID_t_consommable_IND
+     on t_consommable (idConsommable);
+
+create unique index ID_t_fabriquant_IND
+     on t_fabriquant (idFabriquant);
+
+create unique index ID_t_imprimante_IND
+     on t_imprimante (idImprimante);
+
+create index FKt_creer_IND
+     on t_imprimante (idFabriquant);
+
+create unique index ID_t_utiliser_IND
+     on t_utiliser (idConsommable, idImprimante);
+
+create index FKt_u_t_i_IND
+     on t_utiliser (idImprimante);
 
